1f92c254f95d2a1081d5a8a84f03c1559483f155
Cono52 <oflanac52@gmail.com>
2017-05-22 16:00:29 +0100

    filter items based on category selected + renamed some stuff

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ import classnames from 'classnames'
 
 class Products extends Component {
 
-  render() {
-    const productTitles = this.props.products.map(x => 
-      <div className="prodTitle" key={shortid.generate()}>{x.title}</div>
-    )
-    return (
-      <div>{productTitles}</div>
-    );
+    render() {
+      /*Filter out items that don't match category*/
+      const productCategoryMatches = this.props.products.filter(x =>  {
+        const productCategories = x.categories.map(cats => cats.title)
+        if(productCategories.indexOf(this.props.selectedCat) > -1){
+          return true
+        }
+        else{
+          return false
+        }
+      })
+
+      const productTitles = productCategoryMatches.map(x =>  {
+          return <div className="productTitle" key={shortid.generate()}>{x.title}</div>
+      })
+      return <div className="productTitleContainer ">{(productTitles.length !== 0) ? productTitles : "Nothing in this category today :("}</div>
   }
 }
 
 class CatItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-
     render() {
         let classes = classnames('title', {selectedCat: (this.props.selectedCat === this.props.value)});
         return <div 
@@ -35,7 +39,7 @@ class CatItem extends React.Component {
 class CategoryMenu extends Component {
 
   render() {
-    const titles = this.props.catItems.map(x => 
+    const titles = this.props.categories.map(x => 
       <CatItem 
       key={shortid.generate()} 
       value={x.title}
@@ -56,7 +60,7 @@ class App extends Component {
     super(props)
     this.state = {
       selectedCategory: '',
-      catItems: [],
+      categories: [],
       products: []
     }
   }
@@ -69,7 +73,7 @@ class App extends Component {
   getCategories() {
     return fetch('https://api.gousto.co.uk/products/v2.0/categories')
       .then(data => data.json())
-      .then(data => this.setState({catItems: this.state.catItems.concat(data.data)}));
+      .then(data => this.setState({categories: this.state.categories.concat(data.data)}));
   }
 
 
@@ -89,10 +93,13 @@ class App extends Component {
       <div className="App">
         <CategoryMenu 
         selectedCat={this.state.selectedCategory} 
-        catItems={this.state.catItems} 
+        categories={this.state.categories} 
         catClick={this.handleCategoryClick.bind(this)}
         ></CategoryMenu>
-        <Products products={this.state.products}></Products>
+        <Products 
+        selectedCat={this.state.selectedCategory} 
+        products={this.state.products}
+        ></Products>
       </div>
     );
   }
